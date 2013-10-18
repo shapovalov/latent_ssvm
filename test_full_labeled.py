cbe9d739509205b1d915fd637a7de56c85a00844
shapovalov <shapovalov@graphics.cs.msu.ru>
2013-10-18 17:25:58 +0400

    CRF factory added

--- test_full_labeled.py
+++ test_full_labeled.py
@@ -14,13 +14,20 @@ from common import compute_error
 
 # testing with full labeled train sets
 
+def synthetic_edge_crf_factory():
+    return EdgeCRF(n_states=10, n_features=10, n_edge_features=2,
+                  inference_method='gco')
 
-def syntetic():
+def syntetic(peer):
     # train model on a single set
     models_basedir = 'models/syntetic/'
-    crf = EdgeCRF(n_states=10, n_features=10, n_edge_features=2,
-                  inference_method='gco')
-    clf = OneSlackSSVM(crf, max_iter=10000, C=0.01, verbose=2,
+    #crf = EdgeCRF(n_states=10, n_features=10, n_edge_features=2,
+    #              inference_method='gco')
+    #module = peer.config.get("model.factory.module")
+    #func = peer.config.get("model.factory.function")
+    #crf = getattr(__import__(module), func)()
+    
+    clf = OneSlackSSVM(peer, max_iter=10000, C=0.01, verbose=2,
                        tol=0.1, n_jobs=4, inference_cache=100)
 
     X, Y = load_syntetic(1)
